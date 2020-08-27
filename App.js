21adbab3708fc70ca20984e763f551028bc67aaf
nomisrevol <vdn1999bxvp@gmail.com>
2020-08-28 06:53:58 +0700

    Implemented SignUp container

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
+import React, { useState } from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import SignUp from './src/containers/SignUp'
+import Home from './src/containers/Home'
 export default function App () {
+  /* eslint-disable no-unused-vars */
+  const [isAuthorized, setIsAuthorized] = useState(false)
+  /* eslint-enable no-unused-vars */
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {isAuthorized ? <Home /> : <SignUp />}
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1
   }
 })
