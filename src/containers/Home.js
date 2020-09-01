560b69886f5911302e906c128985ef00df235c50
ltta18 <thuylinh7720@gmail.com>
2020-09-01 22:20:21 +0700

    Ads Box

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,29 +1,59 @@
 import React from 'react'
-import { SafeAreaView, View, Image, StyleSheet, Text, ScrollView } from 'react-native'
+import { SafeAreaView, View, Image, StyleSheet, Text, ScrollView, TouchableOpacity, FlatList } from 'react-native'
 import phongvuIcon from '../../assets/pv-icon.png'
 import phongvuLogo from '../../assets/pv-logo.png'
 import { Feather } from '@expo/vector-icons'
+import { TextInput } from 'react-native-paper'
+
+const introUrl = [
+  {
+    id: '1',
+    url: require('../../assets/intro-home.png')
+  },
+  {
+    id: '2',
+    url: require('../../assets/intro-home.png')
+  },
+  {
+    id: '3',
+    url: require('../../assets/intro-home.png')
+  }
+]
+
+const IntroItem = ({ item }) => {
+  return (
+    <Image source={item.url} style={styles.introItem}/>
+  )
+}
 
 export default function Home () {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <View>
-          <Image source={phongvuLogo} />
+          <Image source={phongvuLogo} style={styles.logo}/>
           <Text style={styles.subtitle}>Xin chào, Nam!</Text>
         </View>
         <Feather name='shopping-cart' size={25} color="#1434C3" style={styles.cart} />
       </View>
       <View style={styles.searchBar}>
         <Feather name='search' size={15} color="#1434C3" />
-        <Text style={styles.subtitle}>Bạn cần tìm mua gì?</Text>
+        <TextInput
+          style={[styles.subtitle, styles.searchInput]}
+          placeholder="Bạn cần tìm mua gì?"
+          underlineColor='transparent'
+          underlineColorAndroid='transparent'
+          autoCorrect={false}
+        >
+        </TextInput>
       </View>
       <ScrollView style={styles.body}>
-        {/* <FlatList
+        <FlatList
           data={introUrl}
-          renderItem={introItem}
-          keyExtractor={(item) => item.id}
-        /> */}
+          renderItem={item => <IntroItem item={item.item} />}
+          keyExtractor={item => item.id}
+          horizontal={true}
+        />
       </ScrollView>
       <View style={styles.upperImage}>
         <Image source={phongvuIcon} />
@@ -47,13 +77,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20
   },
+  logo: {
+    marginBottom: 5
+  },
   searchBar: {
     flex: 1 / 9,
     backgroundColor: '#F1FDFD',
     borderRadius: 20,
     flexDirection: 'row',
     alignItems: 'center',
-    paddingLeft: 20
+    paddingLeft: 20,
+    marginLeft: 15,
+    marginRight: 15
+  },
+  searchInput: {
+    backgroundColor: 'transparent'
   },
   body: {
     flex: 7 / 9
@@ -61,7 +99,7 @@ const styles = StyleSheet.create({
   upperImage: {
     position: 'absolute',
     top: -15,
-    left: -15,
+    left: -15
   },
   title: {
     color: '#1434C3',
@@ -69,12 +107,15 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     color: '#1434C3',
-    fontWeight: '200',
-    marginTop: 5
+    fontWeight: '200'
   },
   introItem: {
     width: 280,
-    height: 130,
-    borderRadius: 20
+    height: 150,
+    // borderRadius: 20,
+    marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    resizeMode: 'contain'
   }
 })
