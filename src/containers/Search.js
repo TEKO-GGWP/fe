c674cfe62b296b033a17e3e434277ff9c1b0d6c3
ltta18 <thuylinh7720@gmail.com>
2020-09-09 15:49:59 +0700

    Add icons

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,5 +1,14 @@
 import React from 'react'
-import { SafeAreaView, View, Text, TouchableOpacity, FlatList } from 'react-native'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  TextInput
+} from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 // const historyItem = (val) => {
 //   return (
@@ -18,6 +27,26 @@ export default function Search () {
   return (
     <SafeAreaView>
       <View>
+        <View style={styles.searchBar}>
+          <Feather
+            name='search'
+            size={15}
+            color="#1434C3"
+          />
+          <TextInput
+            style={
+              [
+                styles.subtitle,
+                styles.searchInput
+              ]
+            }
+            placeholder="Bạn cần tìm mua gì?"
+            underlineColor='transparent'
+            underlineColorAndroid='transparent'
+            autoCorrect={false}
+          >
+          </TextInput>
+        </View>
         <Text>Lịch sử tìm kiếm</Text>
         <TouchableOpacity>
           <Text>Xóa hết</Text>
@@ -29,7 +58,28 @@ export default function Search () {
       </View>
       <View>
         <Text>Gợi ý dành riêng cho bạn</Text>
+        <Feather
+          name="chevron-left"
+          size={40}
+        />
+        <Feather
+          name="chevron-right"
+          size={40}
+        />
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  searchBar: {
+    flex: 1 / 9,
+    backgroundColor: '#F1FDFD',
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 20,
+    marginLeft: 5,
+    marginRight: 5
+  }
+})
