a7bdb65f81017f7cbbb9da90238a3ad0ddfa4ef2
tthandb <tthandb@gmail.com>
2020-09-19 07:08:08 +0700

    tam duoc lan 2

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -20,10 +20,10 @@ import { actFetchHotDealRequest, actFetchItChoiceRequest, actFetchOfficerChoiceR
 
 const Home = (props) => {
   const handleGoTop = () => {
-    // this.scroll.scrollTo({
-    //   y: 0,
-    //   animated: true
-    // })
+    this.scroll.scrollTo({
+      y: 0,
+      animated: true
+    })
   }
   const { category } = props
   useEffect(() => {
@@ -74,10 +74,7 @@ const Home = (props) => {
       </View>
 
       <ScrollView style={styles.body}
-      //  ref={(c) => {
-      //   this.scroll = c
-      // }
-      >
+        ref={(c) => { this.scroll = c }}>
         <FlatList
           data={introUrl}
           renderItem={item => <IntroItem item={item.item} />}
